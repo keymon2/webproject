1405aa94adedbf0ceceb9aa3e0af781db4436010
seung won lee <seungwonlee@seungui-MacBookAir.local>
2021-05-25 12:46:40 +0900

    commit asd

--- src/views/new/user/album/Album.js
+++ src/views/new/user/album/Album.js
@@ -4,13 +4,23 @@ import { Gallery, Item } from 'react-photoswipe-gallery'
 import React from 'react'
 
 const Album = (props) => {
+
+    //
+
+
+
+
+
+
+
+
     return (
         <Gallery>
             <Item
                 original="http://img.hani.co.kr/imgdb/resize/2019/0606/00500014_20190606.JPG"
                 thumbnail="http://img.hani.co.kr/imgdb/resize/2019/0606/00500014_20190606.JPG"
-                width="1024"
-                height="768"
+                width="700"
+                height="123"
             >
                 {({ ref, open }) => (
                     <img classname='small2' ref={ref} onClick={open} src="http://img.hani.co.kr/imgdb/resize/2019/0606/00500014_20190606.JPG" />
