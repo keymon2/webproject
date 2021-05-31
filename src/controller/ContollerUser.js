c55f6645d83890148ca30bcf52967121e9ce2fad
seung won lee <seungwonlee@seungui-MacBookAir.local>
2021-05-31 12:36:30 +0900

    complete album

--- src/controller/ContollerUser.js
+++ src/controller/ContollerUser.js
@@ -63,30 +63,3 @@ export async function updateUser(credentials){
     })
     .then(res => res.json())
 }
-export async function findAlbum(){
-  const Token = getToken();
-  if(Token == false) return false;
-  return fetch('http://localhost:4502/api/Album/find',{
-      method: 'GET',
-      headers : {
-          'Content-Type': 'application/json',
-          Authorization : "Bearer " + Token,
-      },
-    })
-    .then(res => res.json())
-}
-
-export async function insertAlbum(credentials){
-  const Token = getToken();
-  if(Token == false) return false;
-  console.log(JSON.stringify(credentials))
-  return fetch('http://localhost:4502/api/Album/insert',{
-      method: 'POST',
-      headers : {
-          'Content-Type': 'application/json',
-          Authorization : "Bearer " + Token,
-      },
-      body: JSON.stringify(credentials)
-    })
-    .then(res => res.json())
-}
--- src/views/new/user/album/Album.js
+++ src/views/new/user/album/Album.js
@@ -17,21 +17,27 @@ import 'photoswipe/dist/photoswipe.css'
 import 'photoswipe/dist/default-skin/default-skin.css'
  
 import { Gallery, Item } from 'react-photoswipe-gallery'
-import { findAlbum} from "../../../../controller/ContollerUser.js"
+
 import "./style.css"
+import {insertAlbum,findAlbum} from "../../../../controller/controllerAlbum"
+import { arrayOf } from 'prop-types'
 
 class Sample extends React.Component {
     constructor(props){
         super (props);
         this.state = {
             image: [],
-            tag: "전체"
+            tag: "전체",
+            list: [],
+            ele: ""
         };
         this.tagChange = this.tagChange.bind(this)
     }
     async componentDidMount(){
         const data = await findAlbum()
         const image = data.data.image;
+        console.log(image)
+        console.log(typeof(image))
         this.setState({image : image});
         
     }
@@ -39,10 +45,48 @@ class Sample extends React.Component {
        console.log("asdasd")
         this.setState({tag: Tag})
    }
+   addList = ()=>{
+       const list = this.state.list
+       list.push(this.state.ele)
+       this.setState( { 
+           list: list,
+           ele: ""
+        })
+   }
+   listRemove= (id) =>{
+        const list = this.state.list
+        for(let i = 0; i < list.length; i++){
+            if(list[i] === id){
+                list.splice(i,1);
+                break;
+            }
+        }
+        this.setState({list: list})
+   }
+   submit= async() => {
+        const list = this.state.list
+        var listSubmit=[];
+        list.map(ele => {
+            listSubmit.push({
+                src: ele,
+                location: "personal"
+            })
+        })
+        const response = await insertAlbum(listSubmit)
+        if( response.success == false){
+            alert("땡!")
+        }
+        const image = await findAlbum();
+
+       this.setState({
+           image: image.data.image,
+           list:[],
+           ele: ""
+       })
+   }
     render() {
-        console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-       const image = this.state.image.map( img =>{
-            const Tag = this.state.tag
+        const image_t = this.state.image.map( img =>{
+            var Tag = this.state.tag
             if(Tag != "전체"){
                 if(img.location == Tag){
                     return (
@@ -90,14 +134,34 @@ class Sample extends React.Component {
                     <list >       
                     <Gallery  >
                         <a>
-                        {image}
+                        {image_t}
                         </a>
                     </Gallery>
-                    </list>     
+                    </list> 
+                    <br/>
+                    <CCard>
+                        <input type="text" name="name" onChange={(e)=>this.setState({ele: e.target.value})}/> 
+                        <list>
+                            {
+                                    this.state.list.map(ele => {
+                                        return(
+                                            <div>
+                                                <a>{ele}</a>
+                                                <button className = "button" onClick={(e)=>this.listRemove(ele)} > 삭제 </button>
+                                            </div>
+                                        )
+                                    })
+                                
+                            }
+                        </list>
+                        <button className="button" onClick = {()=>this.addList()}> 추가 </button>
+                        <button className="button" onClick ={() => this.submit()}> 올리기 </button>
+                    </CCard>
                 </div>
             );}
 
     
 };
+
  
 export default Sample;
