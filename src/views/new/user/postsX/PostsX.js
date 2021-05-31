548979578f11740b74efed2874143ceb688c436b
홍성현 <hongseonghyeon@hongseonghyeon-ui-MacBookPro.local>
2021-05-31 12:45:29 +0900

    postx draft

--- src/views/new/user/postsX/PostsX.js
+++ src/views/new/user/postsX/PostsX.js
@@ -1,48 +1,36 @@
-import React from 'react';
-import { CCard,
-    CCardBody,
-    CCardHeader,
-    CCol,
-    CRow } from '@coreui/react'
+import React from 'react'
+import Board from 'react-trello'
 
-const PostsX = () => {
-    const postsList =
-        [
-            {
-                head: "이번주 여행계획",
-                body: "북한산을 찍고, 남한강, 제주도 를 가고 싶다.~~",
-                day : "04/15 4명 모집합니다."
-            },
-            {
-                head: "4월 21 러닝",
-                body: "벚꽃이 다 진관계로 우리는 뜁니다. 용산역 출발 여의도 도착",
-                day: "04/27 4명 모집! "
-            }
-        ];
-    return (
-        <>
-            <CRow>
-                {
-                    postsList.map((element)=>{
-                        return(
-                    <CCol xs="12" sm="6" md="4">
-                            <CCard>
-                                <CCardHeader>
-                                {element.head}
-                                </CCardHeader>
-                                <CCardBody>
-                                {element.body}
-                                {element.day}
-                                </CCardBody>
-                            </CCard>
-                    </CCol>
-                            
-                        )
-                    })
-                }
-            </CRow>
-        </>
-    );
-};
+const data = {
+    lanes: [
+      {
+        id: 'lane1',
+        title: 'Planned Tasks',
+        label: '3/3',
+        collapsibleLanes: true,
+        cards: [
+          {id: 'Card1', title: 'Running', description: '벚꽃이 다 진관계로 우리는 뜁니다. 용산역 출발 여의도 도착', label: '4/27 4명 모집', draggable: true, editable: true},
+          {id: 'Card2', title: '등산의 목적', description: '북한산을 찍고, 남한강, 제주도 를 가고 싶다.~~', label: '04/27 4명 모집', metadata: {sha: 'be312a1'},editable: true},
+          {id: 'Card3', title: 'NextLevel', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'},canAddLanes: true,editable: true}
+        ]
+      },
+      {
+        id: 'lane2',
+        title: '고민 중',
+        label: '0/0',
+        cards: []
+      },
+      {
+        id: 'lane3',
+        title: 'Completed',
+        label: '0/0',
+        cards: []
+      }
+    ]
+  }
 
-export default PostsX;
+export default class App extends React.Component {
+  render() {
+    return <Board data={data} />
+  }
+}
